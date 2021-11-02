5a802e586666071d4dc1f63aa22b59a2b1bcd034
Steven Chen <steven.chen.121@gmail.com>
2021-11-02 16:59:10 -0400

    Completed math. Just adding charting.

---
+++ script.js
@@ -0,0 +1,58 @@
+const app = {};
+
+app.collatzArray = [];
+app.form = document.querySelector('.input-form');
+app.number = document.querySelector('#user-number');
+
+app.submitHandler = () => {
+
+    app.form.addEventListener('submit', (e) => {
+        app.collatzArray = [];
+        e.preventDefault();
+        const firstNum = parseInt(app.number.value);
+
+        app.collatzArray.push(firstNum);
+        app.nextNum(firstNum);
+    });
+}
+
+// Recursive function to grab the next number until it loops indefinitely 
+app.nextNum = (previousNumber) => {
+    let nextNumber = 0;
+
+    if (previousNumber % 2 === 0) {
+        nextNumber = previousNumber / 2;
+    } else {
+        nextNumber = previousNumber * 3 + 1;
+    }
+
+    if (app.collatzArray.length > 6) {
+        if (app.collatzCatch(app.collatzArray.length - 1) === false) {
+            app.collatzArray.push(nextNumber);
+            app.nextNum(nextNumber);
+        } else {
+            console.log(app.collatzArray);
+        }
+    } else {
+        app.collatzArray.push(nextNumber);
+        app.nextNum(nextNumber);
+    }
+}
+
+// Catch statement if the last 6 numbers have looped. It will only loop at 4 2 1 and then it will loop indefinitely.
+app.collatzCatch = (lastNum) => {
+    if (app.collatzArray[lastNum] === app.collatzArray[lastNum - 3] &&
+        app.collatzArray[lastNum - 1] === app.collatzArray[lastNum - 4] &&
+        app.collatzArray[lastNum - 2] === app.collatzArray[lastNum - 5]
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+app.init = () => {
+    app.submitHandler();
+};
+
+app.init();
